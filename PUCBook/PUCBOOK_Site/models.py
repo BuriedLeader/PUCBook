01dd1cceed944a2e21a1fa66abd55cae423cf709
VINICIUS MACHADO DA ROCHA VIANA <e2111343@BATMAN.LAB-GRAD>
2022-09-28 12:07:22 -0300

    Versao com banco de dados

--- PUCBook/PUCBOOK_Site/models.py
+++ PUCBook/PUCBOOK_Site/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Mensagem(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    texto = models.TextField()
+    data_envio = models.DateTimeField(blank=True, null=True)
+
+    def enviar(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.texto
+    
