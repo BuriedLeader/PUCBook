0579e82e3a18a6ed559a61a738f10bb3382d8ac0
BuriedLeader <buriedleader@gmail.com>
2022-10-16 11:29:11 -0300

    backup rapido

--- PUCBook/PUCBOOK_Site/models.py
+++ PUCBook/PUCBOOK_Site/models.py
@@ -1,44 +1,18 @@
+from email.policy import default
+from operator import mod
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Mensagem(models.Model):
-    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    texto = models.TextField()
-    data_envio = models.DateTimeField(blank=True, null=True)
-
-    def enviar(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.texto
-
-class CaixaComInformacoes(models.Model):
-    titulo = "Titulo" + "\n"
-    texto = models.TextField()
-
-    def __str__(self):
-        return titulo + self.texto
-
-class Carona(models.Model):
-    meio_de_transporte = ""
-    valor = 0
-    rota = ""
-
-class Grupo(models.Model):
-    tipo = ""
-    quantidade_membros = 0
-    adm = ""
-
-
 class Usuario(models.Model):
-    idade = models.IntegerField(default=0)
-    curso = models.CharField(max_length = 200)
-    aniversario = models.CharField(max_length = 10)
-    periodo = models.IntegerField(default=0)
+    nome = models.CharField(max_length = 100)
+    idade = models.IntegerField()
+    aniversario = models.DateField()
+    periodo = models.IntegerField()
     email = models.CharField(max_length = 200)
     ponto_de_encontro = models.CharField(max_length = 200)
+    def __str__(self):
+        return self.nome
 
 class Curso(models.Model):
     nome = models.CharField(max_length = 100)
--- PUCBook/PUCBook/settings.py
+++ PUCBook/PUCBook/settings.py
@@ -107,9 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10n = True
 
 USE_TZ = True
 
